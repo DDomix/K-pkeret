803ee042e84fe438df3231bc23e712e53d6be086
DDomix <dominik.papp1010@gmail.com>
2022-09-28 14:19:04 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import './style.css';
 
+function kep(){
+    let link=document.getElementById("url").value;
+    document.getElementById("kep").src = link;
+}
+function szelessegvaltozas(){
+    let szelesseg=document.getElementById('width').value;
+    document.getElementById('kep').style.width=szelesseg+"px";
+}
+function kepkeret(){
+    let vastag=document.getElementById('vastagsag').value;
+    document.getElementById('kep').style.borderWidth = vastag + "px";
+    console.log(vastag);
+}
+function szin(){
+    let szinek =document.getElementById('szin').value;
+    document.getElementById('kep').style.borderColor=szinek;
+}
+document.getElementById('url').addEventListener('change', kep);
+document.getElementById('width').addEventListener('change', szelessegvaltozas);
+document.getElementById('vastagsag').addEventListener('change', kepkeret);
+document.getElementById('szin').addEventListener('input', szin);
